8be2215915508993e2fc71b989f8f9172e0381bd
Bahman Movaqar <Bahman@BahmanM.com>
2016-07-14 02:54:10 +0430

    * rename utils to range utils * add 'all' and 'contains' operations * fix a bug with 'groupBy' * remove top-level 'groupBy' function * rename 'GroupBy' template to 'groupBy' and make it public

--- source/rangeutils.d
+++ source/rangeutils.d
@@ -1,36 +1,16 @@
 /**
- * A collection of utility functions and templates.
+ * A collection of range utility functions and templates.
  * 
  * Authors: Bahman Movaqar <Bahman AT BahmanM.com>
  * Copyright: Bahman Movaqar 2016-.
  */
-module utils;
+module rangeutils;
 
 private import std.stdio;
 private import std.algorithm.iteration;
 private import std.range;
 private import std.typecons;
 private import std.traits;
-
-private template GroupBy(alias clause, Range)
-   if (isInputRange!Range)
-{
-  static alias ElemType = ElementType!Range;
-  static alias GroupKeyType = typeof(clause(ElemType.init));
-  static alias GroupByAAType = ElemType[][GroupKeyType];
-  
-  /***/
-  auto groupBy(Range r)
-  {
-    GroupByAAType result;
-    return r.fold!(
-      function GroupByAAType(GroupByAAType acc, string val) {
-        acc[clause(val)] ~= val;
-        return acc;
-      }
-    )(result);
-  }
-}
 
 /**
  * Groups a given range into partitions based on a given 'groupBy' clause.
@@ -55,10 +35,24 @@ private template GroupBy(alias clause, Range)
  * Returns: an AA of type ElemType[][GroupKeyType]
  * 
  */
-public auto groupBy(alias clause, R)(R r)
-  if (isInputRange!R)
+public template groupBy(alias clause, Range)
+   if (isInputRange!Range)
 {
-  return GroupBy!(clause, R).groupBy(r);
+  static alias ElemType = ElementType!Range;
+  static alias GroupKeyType = typeof(clause(ElemType.init));
+  static alias GroupByAAType = ElemType[][GroupKeyType];
+  
+  // ditto
+  auto groupBy(Range r)
+  {
+    GroupByAAType result;
+    return r.fold!(
+      function GroupByAAType(GroupByAAType acc, ElemType val) {
+        acc[clause(val)] ~= val;
+        return acc;
+      }
+    )(result);
+  }
 }
 
 unittest {
@@ -70,36 +64,58 @@ unittest {
   assert(result1[6] == ["bahman"]);
   assert(result1[2].length == 2);
   assert(
-    all!(
-      (s => canFind(result1[2], s)), 
-      ["hi", "by"]
+    result1[2].all!(
+      s => ["hi", "by"].contains(s)
     )
   );
   assert(result1[5].length == 2);
   assert(
-    all!(
-      (s => canFind(result1[5], s)), 
-      ["nahid", "sunny"]
+    result1[5].all!(
+      s => ["nahid", "sunny"].contains(s)
     )
   );
   
   // group integers by mod 2
-  import std.algorithm.searching : all, canFind;
   const result2 = [1, 2, 3, 4, 5, 6, 7]
     .groupBy!(i => i % 2);
   assert(result2.length == 2);
   assert(result2[0].length == 3);
   assert(
-    all!(
-      (i => canFind(result2[0], i)), 
-      [2, 4, 6]
+    result2[0].all!(
+      i => (cast(const(int)[]) [2, 4, 6]).contains(i)
     )
   );
   assert(result2[1].length == 4);
-    assert(
-    all!(
-      (i => canFind(result2[1], i)), 
-      [1, 3, 5, 7]
+  assert(
+    result2[1].all!(
+      i => (cast(const(int)[]) [1, 3, 5, 7]).contains(i)
     )
   );
+}
+
+/**/
+public auto all(alias clause, R)(R r)
+  if (isInputRange!R)
+{
+  return r.fold!(
+    (acc, e) => acc && clause(e)
+  )(true);
+}
+
+unittest {
+  assert([1,3,5].all!(i => i % 2 == 1));
+  assert(![1,2,5].all!(i => i % 2 == 1));
+}
+
+/**/
+public auto contains(R, E)(R r, E obj)
+  if (isInputRange!R && is(E == ElementType!R))
+{
+  return !r.filter!(e => obj == e).empty;
+}
+
+
+unittest {
+  assert([1,2,3].contains(2));
+  assert(![1,2,3].contains(20));
 }
